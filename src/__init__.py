824a0dbada716bade22bfca794694861715b77f9
rayhanenouri <rayhane.nouri1@gmail.com>
2026-08-19 11:21:29 +0100

    simplify package init

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,3 @@
-"""
-NASA C-MAPS Predictive Maintenance Package
-Turbofan engine RUL prediction using XGBoost
-"""
+"""NASA C-MAPS turbofan RUL prediction."""
 from .data_loader import load_data
-from .feature_engineering import FeatureEngineer
-
-__version__ = "1.0.0"
-__author__ = "Rayhane Nouri"
-__all__ = [
-    'load_data',
-    'FeatureEngineer',
-]
+from .feature_engineering import prepare_features
